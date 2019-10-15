45daecccdced8bf1d0606bab173376a839a197ce
JingQian87 <qianjing8758@gmail.com>
2019-10-14 20:53:58 -0400

    up

--- hw2/models.py
+++ hw2/models.py
@@ -4,8 +4,8 @@ Kathy McKeown
 Homework 2: Emotion Classification with Neural Networks - Models File
 Authors: Elsbeth Turcan <eturcan@cs.columbia.edu>
 
-<YOUR NAME HERE>
-<YOUR UNI HERE>
+<Jing Qian>
+<jq2282>
 """
 
 import torch
@@ -14,12 +14,15 @@ import torch.nn.utils.rnn as rnn
 
 
 class DenseNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, embed_dim, output_dim, hidden_dim, weight):
         super(DenseNetwork, self).__init__()
 
         ########## YOUR CODE HERE ##########
         # TODO: Here, create any layers and attributes your network needs.
-        raise NotImplementedError
+        self.embedding = nn.Embedding.from_pretrained(weight)
+        self.dense1 = nn.Linear(embed_dim, hidden_dim) 
+        self.dense2 = nn.Linear(hidden_dim, output_dim)
+        self.relu = nn.ReLU()     
 
     def forward(self, x):
         ########## YOUR CODE HERE ##########
@@ -27,10 +30,15 @@ class DenseNetwork(nn.Module):
         # TODO: (The backward pass will be performed by PyTorch magic for you!)
         # TODO: Your architecture should...
         # TODO: 1) Put the words through an Embedding layer (which was initialized with the pretrained embeddings);
-        # TODO: 2) Take the sum of all word embeddings in a sentence; and
+        x = self.embedding(x)
+        # TODO: 2) Take the sum of all word embeddings in a sentence
+        x = torch.sum(x,dim=1).float()
         # TODO: 3) Feed the result into 2-layer feedforward network which produces a 4-vector of values,
         # TODO: one for each class
-        raise NotImplementedError
+        x = self.dense1(x)
+        x = self.relu(x)
+        x = self.dense2(x)
+        return x
 
 
 class RecurrentNetwork(nn.Module):
